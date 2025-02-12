dbded121e4ddc3806ec9d99aeaf63ac8e2d4701a
nehajeyaram <neha.jeyaram@gmail.com>
2025-02-12 01:37:13 -0500

    added gui features (file upload, additional input types, invalid input detection, etc.)

--- si2dla_gui.py
+++ si2dla_gui.py
@@ -1,10 +1,12 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import scrolledtext
+from tkinter import filedialog
 from ast import literal_eval
-from si2dla import *
+from si2dla_mod import *
 import graphviz
 from PIL import ImageTk, Image
+import re
 
 import os
 import pathlib
@@ -14,14 +16,78 @@ os.environ["PATH"] += os.pathsep + f"{pathlib.Path(__file__).parent.resolve()}/g
 global_image_list = []
 # check for invalid input
 
-def execute_algorithm():
-    D_value = D_entry.get("1.0", "end-1c")
+def read_data(D_value):
+    if bool(re.match(r"\[(\s*\(\s*(\"|\')[^\"\'\[\]]+(\"|\')\s*,\s*(\"|\')[^\"\'\[\]]+(\"|\')\s*\)\s*,)*\s*(\s*\(\s*(\"|\')[^\"\'\[\]]+(\"|\')\s*,\s*(\"|\')[^\"\'\[\]]+(\"|\')\s*\)\s*,?)\]\s*", D_value)):
+        D_list = literal_eval(D_value)
+    elif bool(re.match(r"(([^,\s])+,(\s*[^,\s])+\n)*(([^,\s])+,(\s*[^,\s])+)\s*", D_value)):
+        lines = D_value.splitlines()
+        D_list = []
+        for l in lines:
+            D_list.append(tuple(l.replace(" ", "").split(",")))
+    else:
+        return []
+    
+    return D_list
+
+def read_alphabet(value, D_list):   
+    if value == "r_empty":
+        strs = []
+        for tup in D_list:
+            strs.append(tup[0])
+        return_list = list(set("".join(strs)))
+    elif value == "s_empty":
+        strs = []
+        for tup in D_list:
+            strs.append(tup[1])
+        return_list = list(set("".join(strs)))
+    elif bool(re.match(r"\[(\s*[\"|\']([^\'\"\[\]])+[\"|\'],)*\s*(\s*[\"|\']([^\'\"\[\]])+[\"|\'])\s*\]\s*", value)):
+        return_list = literal_eval(value)
+    elif bool(re.match(r"(\s*([^\'\"\[\]\s])+\s*,)*\s*([^\'\"\[\]\s])+\s*", value)):
+        return_list = value.replace(" ", "").split(",")
+    else: 
+        return []
+
+    return return_list
+
+def execute():
+    D_value= D_entry.get("1.0", "end-1c")
     R_value = R_entry.get("1.0", "end-1c")
     S_value = S_entry.get("1.0", "end-1c")
 
-    D_list = literal_eval(D_value)
-    R_list = R_value.split(",")
-    S_list = S_value.split(",")
+    D_list = read_data(D_value)
+    
+    if D_list == []:
+        print("error reading data.")
+        return
+
+    execute_algorithm(D_list, R_value, S_value)
+
+def file_execute():
+    file_path = filedialog.askopenfilename(title="Select a file:", filetypes=[("Text file", "*.txt")])
+    if file_path:
+        with open(file_path, "r") as file:
+            file_data = file.read()
+            D_list = read_data(file_data)
+            if D_list == []:
+                print("error reading data.")
+                return
+        execute_algorithm(D_list, "r_empty", "s_empty")
+
+def execute_algorithm(D_list, R_value, S_value):
+    if R_value == "":
+        R_value = "r_empty"
+    if S_value == "":
+        S_value = "s_empty"
+
+    R_list = read_alphabet(R_value, D_list)
+    S_list = read_alphabet(S_value, D_list)
+
+    if D_list == []:
+        print("error reading data.")
+        return
+    if R_list == [] or S_list == []:
+        print("error reading alphabets.")
+        return
 
     (T_f, T_g) = si2dla(D_list, R_list, S_list)
 
@@ -44,6 +110,7 @@ def execute_algorithm():
                 e1_label = e1_label + e[1] + ": " + e[2] + "\\n"
             else: 
                 print("error edge drawing T_f.")
+                return
 
         graph_f.edge(e1[0], e1[1], label=e1_label)
 
@@ -80,6 +147,7 @@ def execute_algorithm():
                 e4_label = e[1] + ": " + e[2] + "\\n"
             else: 
                 print("error edge drawing T_g.")
+                return
 
 
         graph_g.edge(e1[0], e1[1], label=e1_label+"\\n")
@@ -90,8 +158,8 @@ def execute_algorithm():
         graph_f.format = 'png'
         graph_g.format = 'png'
         
-        graph_f.render(filename='graph_f').replace('\\', '/')
-        graph_g.render(filename='graph_g').replace('\\', '/')
+        graph_f.render(directory='gui_files', filename='graph_f').replace('\\', '/')
+        graph_g.render(directory='gui_files', filename='graph_g').replace('\\', '/')
 
         img_f = ImageTk.PhotoImage(Image.open("graph_f.png"))
         global_image_list.append(img_f)
@@ -104,6 +172,7 @@ def execute_algorithm():
         imglabel_g.grid(column=3, row=2, padx=16)
 
 
+        
 
 root = Tk()
 root.title("Si2dla Algorithm")
@@ -132,8 +201,10 @@ S_label.grid(column=1, row=5)
 S_entry = Text(mainframe, width=50, height=3)
 S_entry.grid(column=1, row=6, pady=16)
 
-button = Button(mainframe, text="execute", command=execute_algorithm)
-button.grid(column=1, row=9, pady=32)
+button = Button(mainframe, text="execute", command=execute)
+button.grid(column=1, row=9, pady=16)
 
+file_button = Button(mainframe, text="upload text file", command=file_execute)
+file_button.grid(column=1, row=10, pady=32)
 
 root.mainloop()
