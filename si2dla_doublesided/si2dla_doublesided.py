19afdac9b9a8eb056d5b5f8e284377e2551fc8eb
Eli Franz <efranz31@gmail.com>
2026-09-01 22:16:57 -0400

    documented some of si2dla_doublesided and found a different way to find the environment and target states

--- si2dla_doublesided/si2dla_doublesided.py
+++ si2dla_doublesided/si2dla_doublesided.py
@@ -8,12 +8,56 @@ from utility.helper import *
 
 # Helper functions
 
+def suff_2(w):
+    if len(w) < 2 :
+        return w
+    else :
+        return w[-2:]
+
+def check_suffs(w, suffs_2): 
+    b = True
+
+    if len(suffs_2) > 1 :
+        """second to last index"""
+        x = suffs_2[-2]
+
+        if (x.endswith(w) or w.endswith(x)) == False:
+            b = False
+
+    return b
+        
+
+"""T: FST object
+q: state in T
+"""
+def check_qt(T, q):
+    is_qt = True
+    suffs_2 = []
+
+    for tr in T.E   :
+        if tr[3] == q :
+            w = ''
+            w = w.join((tr[2] + T.stout[q]))
+            w = suff_2(w)
+            suffs_2.append(w)
+
+            if not(check_suffs(w, suffs_2)) :
+                is_qt = False
+
+    return is_qt
+
 def suff_1(w):
     """Returns 1-suffix of w"""
     if w == "":
         return ""
     else:
         return w[-1]
+
+def pref_1(w):
+    if w == "" :
+        return ""
+    else :
+        return w[0]
 
 
 def get_OS(T,q):
@@ -45,9 +89,6 @@ def lncat_format(w, v):
     else: 
         print ("Does not end with")
         return w
-        
-
-
 
 # Main algorithm
 
@@ -65,7 +106,6 @@ def si2dla(D,Rho,Sigma):
     print("  stout:\t"+str(T_f.stout)+"\n") # n: state outputs (state, output)
     
     
-    
 
     q1 = T_f.Q[0]   # n: state 1
     q2 = T_f.Q[1]   # n: state 2
@@ -78,44 +118,46 @@ def si2dla(D,Rho,Sigma):
 
     #*** construct_T_g
 
-    Q_g = ["qe","qd", 'qt']
+    Q_g = ["qe","qd", "qt"]
 
     corr = {}
     rroc = {}
 
     remaining_states = [q1, q2, q3]
 
-    if len(T_f.stout[q1]) > 0:
-        corr['qt'] = q1
-        rroc[q1] = 'qt' 
+    "find state with largest os_set"
+
+    if len(OS[q1]) > len(OS[q2]) and len(OS[q1]) > len(OS[q3]) :
+        corr['qd'] = q1
+        rroc[q1] = 'qd'
         remaining_states.remove(q1)
-
-    elif len(T_f.stout[q2]) > 0:
-        corr['qt'] = q2
-        rroc[q2] = 'qt' 
+    elif len(OS[q2]) > len(OS[q1]) and len(OS[q2]) > len(OS[q3]) :
+        corr['qd'] = q2
+        rroc[q2] = 'qd'
         remaining_states.remove(q2)
-
-    elif len(T_f.stout[q3])> 0:
-        corr['qt'] = q3
-        rroc[q3] = 'qt'
+    else :
+        corr['qd'] = q3
+        rroc[q3] = 'qd'
         remaining_states.remove(q3)
 
-    if len(OS[remaining_states[0]]) > len(OS[remaining_states[1]]):
-        corr['qd'] = remaining_states[0]
-        corr['qe'] = remaining_states[1]
-        rroc[remaining_states[0]] = 'qd'
-        rroc[remaining_states[1]] = 'qe'
-    else:
-        corr['qd'] = remaining_states[1]
-        corr['qe'] = remaining_states[0]
-        rroc[remaining_states[1]] = 'qd'
-        rroc[remaining_states[0]] = 'qe'
+    print(remaining_states[0])
 
-    OS[corr['qt']] = {T_f.stout[corr['qt']][0]}
+    if check_qt(T_f, remaining_states[0]) == True :
+        corr['qt'] = remaining_states[0]
+        rroc[remaining_states[0]] = 'qt'
+        remaining_states.remove(remaining_states[0])
+    else :
+        corr['qt'] = remaining_states[1]
+        rroc[remaining_states[1]] = 'qt'
+        remaining_states.remove(remaining_states[1])
+
+    corr['qe'] = remaining_states[0]
+    rroc[remaining_states[0]] = 'qe'
+
+    if len(T_f.stout[corr['qt']]) > 0 :
+        OS[corr['qe']] = {T_f.stout[corr['qt']][0]}
 
     print("OSs for T_f:\t"+str(OS))
-
-    
 
     print("corr:\t\t"+str(corr))
 
@@ -133,15 +175,22 @@ def si2dla(D,Rho,Sigma):
     d_g = {}
 
     for q in Q_g:
-        for s in Sigma:
-            if s in IS['qe']:
-                d_g[(q,s)] = 'qe'
-            if s in IS['qd']:
-                d_g[(q,s)] = 'qd'
-            if s in IS['qt'] and q != 'qd' and q != 'qt':
-                d_g[(q,s)] = 'qt'
-            else:
-                d_g[(q,s)] = 'qd'
+        if q != 'qt':
+            for s in Sigma:
+                if s in IS['qt']:
+                    d_g[(q, s)] = 'qt'
+                if s in IS['qe']:
+                    d_g[(q,s)] = 'qe'
+                if s in IS['qd']:
+                    d_g[(q,s)] = 'qd'
+    
+    for s in Sigma:
+        if s in IS['qe']:
+            d_g[('qt', s)] = 'qe'
+        elif s in IS['qt']:
+            d_g[('qt', s)] = 'qt'
+        elif s in IS['qd']:
+            d_g[('qt', s)] = 'qd'
         
     
     
@@ -153,12 +202,22 @@ def si2dla(D,Rho,Sigma):
     for s in Sigma:
         o_g[('qd', s)] = s
 
+        """
+        d_trs: list of transitions in T that have qd as state input and have s as 1_pref of string output
+        """
+
         d_trs = [tr for tr in T_f.E if tr[0] == corr['qd'] and tr[2][0] == s]
 
+        """each d is a transition that has has corr['qd'] as input state and have s as 1_pref of string output, which
+        means s must also be the input in T_g"""
         #gather all alternations
         for d in d_trs:
 
             print(f'Alternation 1: {d[2]}')
+
+            """
+            find a transition that that has corr['qe'] as input and the same input symbol as d
+            """
 
             env_tr = [tr for tr in T_f.E if tr[0] == corr['qe'] and tr[1] == d[1]]
 
@@ -166,6 +225,10 @@ def si2dla(D,Rho,Sigma):
 
             print(f'Alternation 2: {env_tr[2]}')
 
+            """
+            trg_tr is a list of transitions that have corr[qt] as input state, the same input symbol as d, and the same 
+            output state as env_tr; why do they need to have same output state as env_tr?
+            """
 
             trg_tr = [tr for tr in T_f.E if tr[0] == corr['qt'] and tr[1] == d[1] and tr[3] == env_tr[3]]
 
@@ -173,51 +236,44 @@ def si2dla(D,Rho,Sigma):
 
             print(f"Alternation 3: {trg_tr[2]}")
 
+            """
+            w_d is the output of d, so it's the output of a string that we know starts with s
+            w_e is the output of env_tr, so it's the output of that same string on the env state
+            w_s is w_e(pref_1(w_d)^-1(w_d) )^-1
+            i.e. find change of w_e by removing the top of w_d and then removing the remainder from w_e, exact same as og
+            si2dla
+            """
 
             # handle alternations coming from env state
             w_d = d[2]
             w_e = env_tr[2]
             w_s = lncat_format(w_e, w_d[1:])
-            
-            # if the initial phoneme out of env changes, then it means that s is the target phoneme that must wait for right context
-            if w_s != tuple(s) and w_s != ():
-                o_g[('qe', s)] = 'lambda'
 
+            # if the initial phoneme changes, then it means this phoneme must wait until it sees the right context
+            if w_s != tuple(s) and w_s != ():
+                o_g[('qt', s)] = 'lambda'
             else:
-                o_g[('qe', s)] = w_s
+                o_g[('qt', s)] = w_s
 
             # handle alternations coming from target state
             w_e = trg_tr[2]
 
-
-            # a b c
-            # a b
-            # get longest common suffix, which is just the reverse of longest common prefix
-            w_e_reversed = w_e[::-1]
-            w_d_reversed = w_d[::-1]
-
-            lcp = lcp_list(w_e_reversed, w_d_reversed)
-
-            lcs = lcp[::-1]
-
-
-            # remove lcs from w_e and prepend it to s
-            w_s = lncat_format(w_e, lcs) + tuple(s)
-            print(w_s)
+            # if the SR has the same length as initial UR, then deletion
+            if len(w_e) == len(w_d):
+                w_s = ''
+            
+            else:
+                # all alternations from target state have extra phonemes
+                w_s = w_e[0] + s
 
             
 
-            # remove the suffix
-            # w_s = tuple(char for char in w_e if char not in w_d) + tuple(s)
-
-            o_g[('qt', s)] = w_s
+            o_g[('qe', s)] = w_s
 
         
         if not d_trs:
-            o_g[('qt', s)] = T_f.stout[corr['qt']] + tuple(s)
-            o_g[('qe', s)] = s
-        
-
+            o_g[('qe', s)] = ''.join(T_f.stout[corr['qt']]) + s
+            o_g[('qt', s)] = s
 
 
     E_g = []
@@ -257,6 +313,7 @@ def si2dla(D,Rho,Sigma):
     print("E_f after deletions: "+str(T_f.E)+"\n")
 
     return T_f, T_g
+
 
     #*** modify_T_f
 
